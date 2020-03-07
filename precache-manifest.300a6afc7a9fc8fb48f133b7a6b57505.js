cfebca32fd2d9dcab2abcfbafc7e61282ea5c7fa
Andrew Aiello <andrew@Andrews-MBP.modem>
2020-03-07 17:30:12 +1100

    Updates

---
+++ precache-manifest.300a6afc7a9fc8fb48f133b7a6b57505.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "c98995a205d5cb22b6ff00d6b5566811",
+    "url": "/football-round-up/static/media/youtube.c98995a2.svg"
+  },
+  {
+    "revision": "9297a7117f824020b448c7ef39d97059",
+    "url": "/football-round-up/static/media/twitter.9297a711.svg"
+  },
+  {
+    "revision": "55a728da3b69ef26001de87284c9e565",
+    "url": "/football-round-up/static/media/instagram.55a728da.svg"
+  },
+  {
+    "revision": "d97f3fbb1d9135c84907a1e12a25eefb",
+    "url": "/football-round-up/static/media/facebook.d97f3fbb.svg"
+  },
+  {
+    "revision": "652a39d24f3cf1e3eb8c",
+    "url": "/football-round-up/static/js/runtime~main.eab89a34.js"
+  },
+  {
+    "revision": "e707b83bf0cb7c4d0d44",
+    "url": "/football-round-up/static/js/main.0bcbd925.chunk.js"
+  },
+  {
+    "revision": "ccb4977e4d1e0c539eec",
+    "url": "/football-round-up/static/js/2.783c090b.chunk.js"
+  },
+  {
+    "revision": "e707b83bf0cb7c4d0d44",
+    "url": "/football-round-up/static/css/main.49adee26.chunk.css"
+  },
+  {
+    "revision": "cc77cf62c8f544a56da8dc4870e060b3",
+    "url": "/football-round-up/index.html"
+  }
+];
